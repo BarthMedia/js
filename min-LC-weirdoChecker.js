97f946b127c2f838883cf05edf7dbc599dfb5685
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2023-01-18 16:25:12 +0100

    Create min-LC-weirdoChecker.js

---
+++ min-LC-weirdoChecker.js
@@ -0,0 +1,21 @@
+$('[bmg-element = "Specific Rooms Step"]').each(function () {
+    let o = $(this),
+        t = o.find(".pl_specific-rooms_minus");
+    ($numbers = o.find('[bmg-element="Number"]')),
+        ($button = o.find('[bmg-form="Continue Button"]')).click(() => {
+            "Passed!" != o.attr("bmg-data-custom-requirements-passed", "Passed!")
+                ? ($numbers.each(function () {
+                      gsap.to(this, { color: "crimson", duration: 0.43 });
+                  }),
+                  t.each(function () {
+                      gsap.to(this, { backgroundColor: "crimson", duration: 0.43 });
+                  }))
+                : ($numbers.each(function () {
+                      gsap.to(this, { color: "black", duration: 0.43 });
+                  }),
+                  t.each(function () {
+                      gsap.to(this, { backgroundColor: "black", duration: 0.43 });
+                  }));
+        });
+});
+
